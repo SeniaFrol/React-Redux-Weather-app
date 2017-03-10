eb3ffc36330a48b14c2dde8ffa85ec7b0d7b61f4
Senia <senia.frolov@gmail.com>
2017-03-10 10:52:56 +0300

    App is ready

---
+++ README.md
@@ -0,0 +1,3 @@
+Hi there!
+
+Simple React Redux Weather App
---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY='441fff83379204f33b7c2e34cab3fc88';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
